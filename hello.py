861a1b52fbb881f62c708d58da9381ead934951a
NikkoleAshleigh <nikkole.hardy.7.1.21@gmail.com>
2022-09-08 20:18:50 -0400

    added working hello world code

--- hello.py
+++ hello.py
@@ -2,4 +2,8 @@
 def hello_world():
     # WRITE ALL OF YOUR CODE (INDENTED) HERE
     # print out the string 'Hello World!'
-    
+
+    print('Hello World!')
+
+# git config --global user.email "you@example.com"
+# git config --global user.name "Your Name"
